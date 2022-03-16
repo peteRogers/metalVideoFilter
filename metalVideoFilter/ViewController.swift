081899cf516b543c688615c9359463f21861f957
Peter Rogers <p.rogers@gold.ac.uk>
2022-03-16 12:05:33 +0000

    added custom kernel and optical flow

--- metalVideoFilter/ViewController.swift
+++ metalVideoFilter/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 import MetalKit
 import AVFoundation
 import CoreImage.CIFilterBuiltins
+import Vision
 
 class ViewController: UIViewController, MTKViewDelegate{
-   
-
+    
+    private let requestHandler = VNSequenceRequestHandler()
+    
+    var previousImage:CIImage?
+    
     @IBOutlet weak var cameraView: MTKView!{
         
             didSet {
@@ -47,17 +51,19 @@ class ViewController: UIViewController, MTKViewDelegate{
         // Do any additional setup after loading the view.
     }
     
+    
+    
     func draw(in view: MTKView) {
         // grab command buffer so we can encode instructions to GPU
         guard let commandBuffer = metalCommandQueue.makeCommandBuffer() else {
             return
         }
-
+        
         // grab image
         guard let ciImage = currentCIImage else {
             return
         }
-
+        
         // ensure drawable is free and not tied in the preivous drawing cycle
         guard let currentDrawable = view.currentDrawable else {
             return
@@ -75,7 +81,7 @@ class ViewController: UIViewController, MTKViewDelegate{
                               commandBuffer: commandBuffer,
                               bounds: newImage.extent,
                               colorSpace: CGColorSpaceCreateDeviceRGB())
-
+        
         // register drawwable to command buffer
         commandBuffer.present(currentDrawable)
         commandBuffer.commit()
@@ -102,14 +108,14 @@ class ViewController: UIViewController, MTKViewDelegate{
         let falseColor = CIFilter(name:"CIFalseColor", parameters: [kCIInputImageKey: maskImage, "inputColor0": CIColor(color: UIColor.init(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.9)),"inputColor1": CIColor(color: UIColor.init(red: 0.8, green: 0.8, blue: 0.9, alpha: 0.9)),])!
         
         let halftone2 = CIFilter(name:"CIKaleidoscope", parameters: [kCIInputImageKey: maskImage])!
-       // let glass = CIFilter(name:"CIGlassDistortion", parameters: [kCIInputImageKey: halftone2.outputImage, "inputTexture": halftone2.outputImage, "inputScale": 20])!
+        // let glass = CIFilter(name:"CIGlassDistortion", parameters: [kCIInputImageKey: halftone2.outputImage, "inputTexture": halftone2.outputImage, "inputScale": 20])!
         halftone2.setValue(CIVector(x: maskImage.extent.width/2, y: maskImage.extent.height/2), forKey: "inputCenter")
-       // halftone2.setValue(10, forKey: "inputAngle")
+        // halftone2.setValue(10, forKey: "inputAngle")
         halftone2.setValue(5, forKey: "inputCount")
         
         let c = CIFilter(name: "CISunbeamsGenerator",parameters: [:])
         currentCIImage = halftone2.outputImage
-       
+        
     }
     
     func setupCaptureSession() {
@@ -133,10 +139,10 @@ class ViewController: UIViewController, MTKViewDelegate{
             self.session?.addOutput(output)
             output.connections.first?.videoOrientation = .landscapeLeft
             self.session?.startRunning()
-           
+            
         }
     }
-
+    
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         // Delegate method not implemented.
         print("size changed some how")
@@ -146,7 +152,66 @@ class ViewController: UIViewController, MTKViewDelegate{
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         // Grab the pixelbuffer frame from the camera output
+        let sample:CIImage!
         guard let pixelBuffer = sampleBuffer.imageBuffer else { return }
-        processVideoFrame(pixelBuffer)
+        let request = CIImage(cvPixelBuffer: pixelBuffer).oriented(.right)
+        
+        var previousImage:CIImage?
+        if (self.previousImage == nil)
+        {
+            self.previousImage = request
+        }
+        let visionRequest = VNGenerateOpticalFlowRequest(targetedCIImage: request, options: [:])
+        
+        do {
+            try self.requestHandler.perform([visionRequest], on: self.previousImage!)
+            
+            if let pixelBufferObservation = visionRequest.results?.first
+            {
+                print(pixelBufferObservation)
+                sample = CIImage(cvImageBuffer: pixelBufferObservation.pixelBuffer)
+                let ciFilter = OpticalFlowVisualizerFilter()
+                ciFilter.inputImage = sample
+                //ciFilter.inputTime = 2.5
+                //ciFilter.inputImage = sample
+                let fil = CIFilter(name: "CIGaussianBlur", parameters: [kCIInputImageKey:ciFilter.outputImage, kCIInputRadiusKey: 40])
+                currentCIImage = fil?.outputImage
+                
+               // currentCIImage = ciFilter.outputImage
+            }
+        } catch {
+            print(error)
+        }
+        // store the previous image
+        self.previousImage = request
+        
+
+        
+       
+    }
+}
+
+class OpticalFlowVisualizerFilter: CIFilter {
+    var inputImage: CIImage?
+    
+    let callback: CIKernelROICallback = {
+            (index, rect) in
+                return rect
+            }
+    
+    static var kernel: CIKernel = { () -> CIKernel in
+        let url = Bundle.main.url(forResource: "myshader",
+                                  withExtension: "ci.metallib")!
+        let data = try! Data(contentsOf: url)
+        
+        return try! CIKernel(functionName: "HDRZebra",
+                                  fromMetalLibraryData: data)
+    }()
+
+    override var outputImage : CIImage? {
+        get {
+            guard let input = inputImage else {return nil}
+            return OpticalFlowVisualizerFilter.kernel.apply(extent: input.extent, roiCallback: callback, arguments: [input, 0.0, 100, 10.0, 30.0])
+        }
     }
 }
---
+++ metalVideoFilter/myshader.ci.metal
@@ -0,0 +1,57 @@
+// MyKernels.ci.metal
+#include <CoreImage/CoreImage.h> // includes CIKernelMetalLib.h
+using namespace metal;
+
+extern "C" {
+    namespace coreimage {
+        float4 HDRZebra (sampler image, float minLen, float maxLen, float size, float tipAngle){
+       // float4 HDRZebra (sampler image){
+            //return s.sample(s.coord());
+            float4 s = image.sample(image.coord());
+            float2 vector = s.rg - 0.5;
+            float len = length(vector);
+            float H = atan2(vector.y, vector.x);
+            H *= 3.0/3.1415926;
+            float i = floor(H);
+            float f = H-i;
+            float a = f;
+            float d = 1.0 - a;
+            float4 c;
+            if (H<-3.0) c = float4(0, 1, 1, 1);
+            else if (H<-2.0) c = float4(0, d, 1, 1);
+                else if (H<-1.0) c = float4(a, 0, 1, 1);
+                else if (H<0.0)  c = float4(1, 0, d, 1);
+                else if (H<1.0)  c = float4(1, a, 0, 1);
+                else if (H<2.0)  c = float4(d, 1, 0, 1);
+                else if (H<3.0)  c = float4(0, 1, a, 1);
+                else             c = float4(0, 1, 1, 1);
+            c.rgb *= clamp((len-minLen)/(maxLen-minLen), 0.0,1.0);
+            float tipAngleRadians = tipAngle * 3.1415/180.0;
+            float2 dc = image.coord(); // current coordinate
+            float2 dcm = floor((dc/size)+0.5)*size; // cell center coordinate
+            float2 delta = dcm - dc; // coordinate relative to center of cell
+                // sample the .xy vector from the center of each cell
+                float4 sm = sample(image, samplerTransform(image, dcm));
+                vector = sm.rg - 0.5;
+                len = length(vector);
+                H = atan2(vector.y,vector.x);
+                float rotx, k, sideOffset, sideAngle;
+                // these are the three sides of the arrow
+                rotx = delta.x*cos(H) - delta.y*sin(H);
+                sideOffset = size*0.5*cos(tipAngleRadians);
+                k = 1.0 - clamp(rotx-sideOffset, 0.0, 1.0);
+                c.rgb *= k;
+                sideAngle = (3.14159 - tipAngleRadians)/2.0;
+                sideOffset = 0.5 * sin(tipAngleRadians / 2.0);
+                rotx = delta.x*cos(H-sideAngle) - delta.y*sin(H-sideAngle);
+                k = clamp(rotx+size*sideOffset, 0.0, 1.0);
+                c.rgb *= k;
+                rotx = delta.x*cos(H+sideAngle) - delta.y*sin(H+sideAngle);
+                k = clamp(rotx+ size*sideOffset, 0.0, 1.0);
+                c.rgb *= k;
+                /// return the color premultiplied
+                c *= s.a;
+                return c;
+        }
+    }
+}
