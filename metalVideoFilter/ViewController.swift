69b6cfe5dd6046a205860e0439a2b95db3d21dcf
Peter Rogers <p.rogers@gold.ac.uk>
2022-03-16 16:13:12 +0000

    sorted and optimised metal shader runs really quickly optical flow

--- metalVideoFilter/ViewController.swift
+++ metalVideoFilter/ViewController.swift
@@ -20,12 +20,12 @@ class ViewController: UIViewController, MTKViewDelegate{
     
     @IBOutlet weak var cameraView: MTKView!{
         
-            didSet {
-                guard metalDevice == nil else { return }
-                setupMetal()
-                setupCoreImage()
-                setupCaptureSession()
-          
+        didSet {
+            guard metalDevice == nil else { return }
+            setupMetal()
+            setupCoreImage()
+            setupCaptureSession()
+            
         }
     }
     
@@ -41,177 +41,45 @@ class ViewController: UIViewController, MTKViewDelegate{
         }
     }
     
-    // The capture session that provides video frames.
     public var session: AVCaptureSession?
     
-    // MARK: - ViewController LifeCycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     
-    
-    func draw(in view: MTKView) {
-        // grab command buffer so we can encode instructions to GPU
-        guard let commandBuffer = metalCommandQueue.makeCommandBuffer() else {
-            return
-        }
-        
-        // grab image
-        guard let ciImage = currentCIImage else {
-            return
-        }
-        
-        // ensure drawable is free and not tied in the preivous drawing cycle
-        guard let currentDrawable = view.currentDrawable else {
-            return
-        }
-        
-        // make sure the image is full screen
-        let drawSize = cameraView.drawableSize
-        let scaleX = drawSize.width / ciImage.extent.width
-        let scaleY = drawSize.height / ciImage.extent.height
-        
-        let newImage = ciImage.transformed(by: .init(scaleX: scaleX, y: scaleY))
-        //render into the metal texture
-        self.ciContext.render(newImage,
-                              to: currentDrawable.texture,
-                              commandBuffer: commandBuffer,
-                              bounds: newImage.extent,
-                              colorSpace: CGColorSpaceCreateDeviceRGB())
-        
-        // register drawwable to command buffer
-        commandBuffer.present(currentDrawable)
-        commandBuffer.commit()
+    private func processVideoFrame(sample:CIImage){
+        let ciFilter = OpticalFlowVisualizerFilter()
+        ciFilter.inputImage = sample
+        let fil = CIFilter(name: "CIGaussianBlur", parameters: [kCIInputImageKey:ciFilter.outputImage!, kCIInputRadiusKey: 15])
+        currentCIImage = fil?.outputImage?.cropped(to: sample.extent)
     }
-    
-    func setupMetal() {
-        metalDevice = MTLCreateSystemDefaultDevice()
-        metalCommandQueue = metalDevice.makeCommandQueue()
-        //cameraView is a metal view!!!
-        cameraView.device = metalDevice
-        cameraView.isPaused = true
-        cameraView.enableSetNeedsDisplay = false
-        cameraView.delegate = self
-        cameraView.framebufferOnly = false
-    }
-    
-    func setupCoreImage() {
-        ciContext = CIContext(mtlDevice: metalDevice)
-    }
-    
-    private func processVideoFrame(_ framePixelBuffer: CVPixelBuffer){
-        
-        let maskImage = CIImage(cvPixelBuffer: framePixelBuffer).oriented(.right)
-        let falseColor = CIFilter(name:"CIFalseColor", parameters: [kCIInputImageKey: maskImage, "inputColor0": CIColor(color: UIColor.init(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.9)),"inputColor1": CIColor(color: UIColor.init(red: 0.8, green: 0.8, blue: 0.9, alpha: 0.9)),])!
-        
-        let halftone2 = CIFilter(name:"CIKaleidoscope", parameters: [kCIInputImageKey: maskImage])!
-        // let glass = CIFilter(name:"CIGlassDistortion", parameters: [kCIInputImageKey: halftone2.outputImage, "inputTexture": halftone2.outputImage, "inputScale": 20])!
-        halftone2.setValue(CIVector(x: maskImage.extent.width/2, y: maskImage.extent.height/2), forKey: "inputCenter")
-        // halftone2.setValue(10, forKey: "inputAngle")
-        halftone2.setValue(5, forKey: "inputCount")
-        
-        let c = CIFilter(name: "CISunbeamsGenerator",parameters: [:])
-        currentCIImage = halftone2.outputImage
-        
-    }
-    
-    func setupCaptureSession() {
-        
-        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
-            fatalError("Error getting AVCaptureDevice.")
-        }
-        guard let input = try? AVCaptureDeviceInput(device: device) else {
-            fatalError("Error getting AVCaptureDeviceInput")
-        }
-        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            guard let self = self else { return }
-            self.session = AVCaptureSession()
-            self.session?.sessionPreset = .high
-            self.session?.addInput(input)
-            
-            let output = AVCaptureVideoDataOutput()
-            output.alwaysDiscardsLateVideoFrames = true
-            output.setSampleBufferDelegate(self, queue: .main)
-            
-            self.session?.addOutput(output)
-            output.connections.first?.videoOrientation = .landscapeLeft
-            self.session?.startRunning()
-            
-        }
-    }
-    
-    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        // Delegate method not implemented.
-        print("size changed some how")
-    }
-    
 }
+
+
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // Grab the pixelbuffer frame from the camera output
-        let sample:CIImage!
         guard let pixelBuffer = sampleBuffer.imageBuffer else { return }
-        let request = CIImage(cvPixelBuffer: pixelBuffer).oriented(.right)
-        
-        var previousImage:CIImage?
+        let request = CIImage(cvPixelBuffer: pixelBuffer).oriented(.up)
         if (self.previousImage == nil)
         {
             self.previousImage = request
         }
         let visionRequest = VNGenerateOpticalFlowRequest(targetedCIImage: request, options: [:])
-        
+        visionRequest.computationAccuracy = .veryHigh
         do {
             try self.requestHandler.perform([visionRequest], on: self.previousImage!)
             
-            if let pixelBufferObservation = visionRequest.results?.first
-            {
-                print(pixelBufferObservation)
-                sample = CIImage(cvImageBuffer: pixelBufferObservation.pixelBuffer)
-                let ciFilter = OpticalFlowVisualizerFilter()
-                ciFilter.inputImage = sample
-                //ciFilter.inputTime = 2.5
-                //ciFilter.inputImage = sample
-                let fil = CIFilter(name: "CIGaussianBlur", parameters: [kCIInputImageKey:ciFilter.outputImage, kCIInputRadiusKey: 40])
-                currentCIImage = fil?.outputImage
-                
-               // currentCIImage = ciFilter.outputImage
+            if let pixelBufferObservation = visionRequest.results?.first{
+                let sample = CIImage(cvImageBuffer: pixelBufferObservation.pixelBuffer)
+                processVideoFrame(sample: sample)
             }
         } catch {
             print(error)
         }
         // store the previous image
         self.previousImage = request
-        
-
-        
-       
     }
 }
 
-class OpticalFlowVisualizerFilter: CIFilter {
-    var inputImage: CIImage?
-    
-    let callback: CIKernelROICallback = {
-            (index, rect) in
-                return rect
-            }
-    
-    static var kernel: CIKernel = { () -> CIKernel in
-        let url = Bundle.main.url(forResource: "myshader",
-                                  withExtension: "ci.metallib")!
-        let data = try! Data(contentsOf: url)
-        
-        return try! CIKernel(functionName: "HDRZebra",
-                                  fromMetalLibraryData: data)
-    }()
-
-    override var outputImage : CIImage? {
-        get {
-            guard let input = inputImage else {return nil}
-            return OpticalFlowVisualizerFilter.kernel.apply(extent: input.extent, roiCallback: callback, arguments: [input, 0.0, 100, 10.0, 30.0])
-        }
-    }
-}
--- metalVideoFilter/myshader.ci.metal
+++ metalVideoFilter/myshader.ci.metal
@@ -49,9 +49,44 @@ extern "C" {
                 rotx = delta.x*cos(H+sideAngle) - delta.y*sin(H+sideAngle);
                 k = clamp(rotx+ size*sideOffset, 0.0, 1.0);
                 c.rgb *= k;
-                /// return the color premultiplied
+
                 c *= s.a;
                 return c;
         }
+        
+        float4 rejig (sample_t s, destination dest)
+        {
+            
+           // float4 s = dest.coord();
+            float2 vector = s.rg - 0.5;
+            float minLength = 1;
+            float maxLength = 100;
+            float len = length(vector);
+            float H = atan2(vector.y, vector.x);
+            H *= 3.0/3.1415926;
+            float i = floor(H);
+            float f = H-i;
+            float a = f;
+            float d = 1.0 - a;
+            float4 c;
+            if (H<-3.0) c = float4(0, 1, 1, 1);
+            else if (H<-2.0) c = float4(0, d, 1, 1);
+                else if (H<-1.0) c = float4(a, 0, 1, 1);
+                else if (H<0.0)  c = float4(1, 0, d, 1);
+                else if (H<1.0)  c = float4(1, a, 0, 1);
+                else if (H<2.0)  c = float4(d, 1, 0, 1);
+                else if (H<3.0)  c = float4(0, 1, a, 1);
+                else             c = float4(0, 1, 1, 1);
+            c.rgb *= clamp((len-minLength)/(maxLength-minLength), 0.0,1.0);
+//            float diagLine = dest.coord().x + dest.coord().y;
+//            float zebra = fract(diagLine/20.0 + time*2.0);
+//            if ((zebra > 0.5) && (s.r > 1 || s.g > 1 || s.b > 1))
+//                return float4(2.0, 0.0, 0.0, 1.0);
+            return c;
+        }
+    
+    
     }
+    
+    
 }
