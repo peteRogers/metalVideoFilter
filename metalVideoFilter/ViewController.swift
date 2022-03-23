e9f43a3590a23203f538fd81d6c77e105c5499f8
Peter Rogers <p.rogers@gold.ac.uk>
2022-03-23 18:15:50 +0000

    tidies and added custom shaders

--- metalVideoFilter/ViewController.swift
+++ metalVideoFilter/ViewController.swift
@@ -11,17 +11,16 @@ import MetalKit
 import AVFoundation
 import CoreImage.CIFilterBuiltins
 
-class ViewController: UIViewController, MTKViewDelegate{
-   
+class ViewController: UIViewController{
+    var k = 0
+    var incer = 1
 
     @IBOutlet weak var cameraView: MTKView!{
-        
             didSet {
                 guard metalDevice == nil else { return }
                 setupMetal()
                 setupCoreImage()
                 setupCaptureSession()
-          
         }
     }
     
@@ -44,101 +43,31 @@ class ViewController: UIViewController, MTKViewDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            //print("Timer fired!")
+            self.k += self.incer
+            if(self.k > 20 || self.k < 1){
+                self.incer = self.incer * -1
+            }
+        }
     }
     
-    func draw(in view: MTKView) {
-        // grab command buffer so we can encode instructions to GPU
-        guard let commandBuffer = metalCommandQueue.makeCommandBuffer() else {
-            return
-        }
-
-        // grab image
-        guard let ciImage = currentCIImage else {
-            return
-        }
-
-        // ensure drawable is free and not tied in the preivous drawing cycle
-        guard let currentDrawable = view.currentDrawable else {
-            return
-        }
+    
+    
+    
+    func processVideoFrame(_ framePixelBuffer: CVPixelBuffer){
+        //gets Image pixels from camera
+        let inputImage = CIImage(cvPixelBuffer: framePixelBuffer).oriented(.right)
         
-        // make sure the image is full screen
-        let drawSize = cameraView.drawableSize
-        let scaleX = drawSize.width / ciImage.extent.width
-        let scaleY = drawSize.height / ciImage.extent.height
+        //creates Kaleidescope
+        let kal = CIFilter(name:"CIKaleidoscope", parameters: [kCIInputImageKey: inputImage])!
+        kal.setValue(CIVector(x: inputImage.extent.width/2, y: inputImage.extent.height/2), forKey: "inputCenter")
+        kal.setValue(self.k, forKey: "inputCount")
         
-        let newImage = ciImage.transformed(by: .init(scaleX: scaleX, y: scaleY))
-        //render into the metal texture
-        self.ciContext.render(newImage,
-                              to: currentDrawable.texture,
-                              commandBuffer: commandBuffer,
-                              bounds: newImage.extent,
-                              colorSpace: CGColorSpaceCreateDeviceRGB())
-
-        // register drawwable to command buffer
-        commandBuffer.present(currentDrawable)
-        commandBuffer.commit()
-    }
-    
-    func setupMetal() {
-        metalDevice = MTLCreateSystemDefaultDevice()
-        metalCommandQueue = metalDevice.makeCommandQueue()
-        //cameraView is a metal view!!!
-        cameraView.device = metalDevice
-        cameraView.isPaused = true
-        cameraView.enableSetNeedsDisplay = false
-        cameraView.delegate = self
-        cameraView.framebufferOnly = false
-    }
-    
-    func setupCoreImage() {
-        ciContext = CIContext(mtlDevice: metalDevice)
-    }
-    
-    private func processVideoFrame(_ framePixelBuffer: CVPixelBuffer){
-        let maskImage = CIImage(cvPixelBuffer: framePixelBuffer).oriented(.right)
-        let kal = CIFilter(name:"CIKaleidoscope", parameters: [kCIInputImageKey: maskImage])!
-        kal.setValue(CIVector(x: maskImage.extent.width/2, y: maskImage.extent.height/2), forKey: "inputCenter")
-        kal.setValue(1000, forKey: "inputCount")
-        currentCIImage = kal.outputImage
-    }
-    
-    func setupCaptureSession() {
+        let filter = SunVisualizerFilter()
+        currentCIImage = filter.outputImage
         
-        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
-            fatalError("Error getting AVCaptureDevice.")
-        }
-        guard let input = try? AVCaptureDeviceInput(device: device) else {
-            fatalError("Error getting AVCaptureDeviceInput")
-        }
-        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            guard let self = self else { return }
-            self.session = AVCaptureSession()
-            self.session?.sessionPreset = .high
-            self.session?.addInput(input)
-            
-            let output = AVCaptureVideoDataOutput()
-            output.alwaysDiscardsLateVideoFrames = true
-            output.setSampleBufferDelegate(self, queue: .main)
-            
-            self.session?.addOutput(output)
-            output.connections.first?.videoOrientation = .landscapeLeft
-            self.session?.startRunning()
-           
-        }
-    }
 
-    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        // Delegate method not implemented.
-        print("size changed some how")
     }
     
-}
-extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // Grab the pixelbuffer frame from the camera output
-        guard let pixelBuffer = sampleBuffer.imageBuffer else { return }
-        processVideoFrame(pixelBuffer)
-    }
 }
---
+++ metalVideoFilter/ViewControllerExtras.swift
@@ -0,0 +1,139 @@
+//
+//  ViewControllerExtras.swift
+//  metalVideoFilter
+//
+//  Created by dt on 23/03/2022.
+//
+
+import Foundation
+import MetalKit
+import AVFoundation
+import CoreImage.CIFilterBuiltins
+
+extension ViewController: MTKViewDelegate,  AVCaptureVideoDataOutputSampleBufferDelegate{
+    func draw(in view: MTKView) {
+        // grab command buffer so we can encode instructions to GPU
+        guard let commandBuffer = metalCommandQueue.makeCommandBuffer() else {
+            return
+        }
+
+        // grab image
+        guard let ciImage = currentCIImage else {
+            return
+        }
+
+        // ensure drawable is free and not tied in the preivous drawing cycle
+        guard let currentDrawable = view.currentDrawable else {
+            return
+        }
+        
+        // make sure the image is full screen
+        let drawSize = cameraView.drawableSize
+        let scaleX = drawSize.width / ciImage.extent.width
+        let scaleY = drawSize.height / ciImage.extent.height
+        
+        let newImage = ciImage.transformed(by: .init(scaleX: scaleX, y: scaleY))
+        //render into the metal texture
+        self.ciContext.render(newImage,
+                              to: currentDrawable.texture,
+                              commandBuffer: commandBuffer,
+                              bounds: newImage.extent,
+                              colorSpace: CGColorSpaceCreateDeviceRGB())
+
+        // register drawwable to command buffer
+        commandBuffer.present(currentDrawable)
+        commandBuffer.commit()
+    }
+    
+    func setupMetal() {
+        metalDevice = MTLCreateSystemDefaultDevice()
+        metalCommandQueue = metalDevice.makeCommandQueue()
+        //cameraView is a metal view!!!
+        cameraView.device = metalDevice
+        cameraView.isPaused = true
+        cameraView.enableSetNeedsDisplay = false
+        cameraView.delegate = self
+        cameraView.framebufferOnly = false
+    }
+    
+    func setupCoreImage() {
+        ciContext = CIContext(mtlDevice: metalDevice)
+    }
+    
+    func setupCaptureSession() {
+        
+        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+            fatalError("Error getting AVCaptureDevice.")
+        }
+        guard let input = try? AVCaptureDeviceInput(device: device) else {
+            fatalError("Error getting AVCaptureDeviceInput")
+        }
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
+            self.session = AVCaptureSession()
+            self.session?.sessionPreset = .high
+            self.session?.addInput(input)
+            
+            let output = AVCaptureVideoDataOutput()
+            output.alwaysDiscardsLateVideoFrames = true
+            output.setSampleBufferDelegate(self, queue: .main)
+            
+            self.session?.addOutput(output)
+            output.connections.first?.videoOrientation = .landscapeLeft
+            self.session?.startRunning()
+           
+        }
+    }
+
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        // Delegate method not implemented.
+        print("size changed some how")
+    }
+    
+
+        func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+            // Grab the pixelbuffer frame from the camera output
+            guard let pixelBuffer = sampleBuffer.imageBuffer else { return }
+            processVideoFrame(pixelBuffer)
+    
+}
+}
+
+class SunVisualizerFilter: CIFilter {
+    var inputImage: CIImage?
+    
+    var inputSunDiameter: CGFloat = 0.1
+    var inputAlbedo: CGFloat = 0.5
+    var inputSunAzimuth: CGFloat = 0
+    var inputSunAlitude: CGFloat = 7
+    var inputSkyDarkness: CGFloat = 1.2
+    var inputScattering: CGFloat = 2.5
+    var inputWidth: CGFloat = 640
+    var inputHeight: CGFloat = 480
+    
+   
+    
+    static var kernel: CIColorKernel = { () -> CIColorKernel in
+        let url = Bundle.main.url(forResource: "skyShader",
+                                  withExtension: "ci.metallib")!
+        let data = try! Data(contentsOf: url)
+        
+        return try! CIColorKernel(functionName: "skyShader",
+                                  fromMetalLibraryData: data)
+    }()
+
+    override var outputImage : CIImage? {
+        get {
+            let extent = CGRect(x: 0, y: 0, width: inputWidth, height: inputHeight)
+            
+            let arguments = [inputWidth, inputHeight, inputSunDiameter, inputAlbedo, inputSunAzimuth, inputSunAlitude, inputSkyDarkness, inputScattering]
+            
+            let final = SunVisualizerFilter.kernel.apply(extent: extent, arguments: arguments)
+            
+            return final
+        }
+    }
+    
+
+
+}
